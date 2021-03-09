d13c3622ada78c635f711cc17d2f7fe9152c08bd
= <=>
2021-03-09 13:37:17 +0300

    Görev listeleme ve ekleme işlemleri yapıldı

--- YSKProje.ToDo.Entities/Concrete/Gorev.cs
+++ YSKProje.ToDo.Entities/Concrete/Gorev.cs
@@ -10,7 +10,7 @@ namespace YSKProje.ToDo.Entities.Concrete
         public string Ad { get; set; }
         public string Aciklama { get; set; }
         public bool Durum { get; set; }
-        public DateTime OlusturulmaTarih { get; set; }
+        public DateTime OlusturulmaTarih { get; set; } = DateTime.Now;
 
         public int AciliyetId { get; set; }
         public Aciliyet Aciliyet { get; set; }
--- YSKProje.ToDo.Web/Areas/Admin/Controllers/GorevController.cs
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/GorevController.cs
@@ -1,18 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YSKProje.ToDo.Business.Interfaces;
+using YSKProje.ToDo.Entities.Concrete;
+using YSKProje.ToDo.Web.Areas.Admin.Models;
 
 namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class GorevController : Controller
     {
-        [Area("Admin")]
+
+        private readonly IGorevService _gorevService;
+        private readonly IAciliyetService _aciliyetService;
+        public GorevController(IGorevService gorevService, IAciliyetService aciliyetService)
+        {
+            _gorevService = gorevService;
+            _aciliyetService = aciliyetService;
+        }
         public IActionResult Index()
         {
             TempData["Active"] = "gorev";
-            return View();
+            List<Gorev> gorevler = _gorevService.GetirHepsi();
+            List<GorevListViewModel> models = new List<GorevListViewModel>();
+            foreach (var item in gorevler)
+            {
+                GorevListViewModel model = new GorevListViewModel
+                {
+                    Aciklama = item.Aciklama,
+                    Aciliyet = item.Aciliyet,
+                    AciliyetId = item.AciliyetId,
+                    Ad = item.Ad,
+                    Durum = item.Durum,
+                    Id = item.Id,
+                    OlusturulmaTarih = item.OlusturulmaTarih
+                };
+                models.Add(model);
+            }
+            return View(models);
+
+        }
+    
+        public IActionResult EkleGorev()
+        {
+           
+            ViewBag.Aciliyetler = new SelectList(_aciliyetService.GetirHepsi(),"Id","Tanim");  
+            TempData["Active"] = "gorev";
+            return View(new GorevAddViewModel());
+        }
+        [HttpPost]
+        public IActionResult EkleGorev(GorevAddViewModel model)
+        {
+            TempData["Active"] = "gorev";
+            if (ModelState.IsValid)
+            {
+                _gorevService.Kaydet(new Gorev()
+                {
+                      Aciklama = model.Aciklama,
+                      Ad = model.Ad,
+                      AciliyetId = model.AciliyetId,
+                });
+                return RedirectToAction("Index");
+            }
+            return View(model);
+
         }
     }
 }
