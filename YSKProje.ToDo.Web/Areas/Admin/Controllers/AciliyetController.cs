669ad639ebe120768e0eecae77a9746f3bbe3ec0
= <=>
2021-03-09 12:09:48 +0300

    Layout active sınıfı işlemleri yapıldı

--- YSKProje.ToDo.Web/Areas/Admin/Controllers/AciliyetController.cs
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/AciliyetController.cs
@@ -19,6 +19,7 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
+            TempData["Active"] = "aciliyet";
             List<Aciliyet> aciliyetler = _aciliyetService.GetirHepsi();
 
             List<AciliyetListViewModel> model = new List<AciliyetListViewModel>();
@@ -33,12 +34,13 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
         }
         public IActionResult EkleAciliyet()
         {
-            
+            TempData["Active"] = "aciliyet";
             return View(new AciliyetAddViewModel());
         }
         [HttpPost]
         public IActionResult EkleAciliyet(AciliyetAddViewModel model)
         {
+            TempData["Active"] = "aciliyet";
             if (ModelState.IsValid)
             {
                 _aciliyetService.Kaydet(new Aciliyet()
@@ -51,7 +53,8 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
         }
         public IActionResult GuncelleAciliyet(int id)
         {
-           var aciliyet= _aciliyetService.GetirIdile(id);
+            TempData["Active"] = "aciliyet";
+            var aciliyet= _aciliyetService.GetirIdile(id);
             AciliyetUpdateViewModel model = new AciliyetUpdateViewModel
             {
                 Id = aciliyet.Id,
@@ -62,10 +65,9 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
         [HttpPost]
         public IActionResult GuncelleAciliyet(AciliyetUpdateViewModel model)
         {
+            TempData["Active"] = "aciliyet";
             if (ModelState.IsValid)
             {
-
-          
             _aciliyetService.Guncelle(new Aciliyet
             {
                 Id = model.Id,
---
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/GorevController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
+{
+    public class GorevController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            TempData["Active"] = "gorev";
+            return View();
+        }
+    }
+}
