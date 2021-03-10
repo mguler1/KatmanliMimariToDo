59ca1d823201f7969cde4980eeea9712b76c1de9
= <=>
2021-03-10 16:54:58 +0300

    AppUser Interface ve Repository eklendi

---
+++ YSKProje.ToDo.DataAccess/Concrete/EntityFrameworkCore/Repositories/EfAppUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Contexts;
+using YSKProje.ToDo.DataAccess.Interfaces;
+using YSKProje.ToDo.Entities.Concrete;
+
+namespace YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Repositories
+{
+    public class EfAppUserRepository : EfGenericRepository<AppUser>, IAppUserDal
+    {
+        public List<AppUser> GetirAdminOlmayanlar()
+        {
+            using var context = new TodoContext();
+        }
+    }
+}
