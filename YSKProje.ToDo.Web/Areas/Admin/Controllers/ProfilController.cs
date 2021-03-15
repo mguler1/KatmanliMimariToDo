960343d49289d429776dbf6036879ca321fc8a87
= <=>
2021-03-15 20:46:57 +0300

    Profil güncelleme formu oluştruldu.Güncelleme metodu yazıldı

---
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/ProfilController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YSKProje.ToDo.Entities.Concrete;
+using YSKProje.ToDo.Web.Areas.Admin.Models;
+
+namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProfilController : Controller
+    {
+      private readonly  UserManager<AppUser> _userManager;
+        public ProfilController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task< IActionResult> Index()
+        {
+          var appuser= await  _userManager.FindByNameAsync(User.Identity.Name);
+            AppUserListViewModel model = new AppUserListViewModel();
+            model.Id = appuser.Id;
+            model.Name = appuser.Name;
+            model.SurName = appuser.Surname;
+            model.Email = appuser.Email;
+            return View(model);
+        }
+    }
+}
--- YSKProje.ToDo.Web/Areas/Admin/Models/AppUserListViewModel.cs
+++ YSKProje.ToDo.Web/Areas/Admin/Models/AppUserListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Models
     public class AppUserListViewModel
     {
         public int Id { get; set; }
+        [Display(Name = "Ad")]
+        [Required(ErrorMessage ="Ad Alanı Boş Geçilemez")]
         public string Name { get; set; }
+        [Display(Name = "Soyad")]
+        [Required(ErrorMessage = "SoyAd Alanı Boş Geçilemez")]
         public string SurName { get; set; }
+        [Display(Name="Email")]
         public string Email { get; set; }
      
     }
