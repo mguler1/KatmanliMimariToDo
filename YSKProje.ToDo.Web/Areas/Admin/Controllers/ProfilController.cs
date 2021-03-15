a5e90437293d69049d27195450a3c78358db9701
= <=>
2021-03-15 22:10:24 +0300

    Profil güncelleme işlemi tamamlandı

--- YSKProje.ToDo.Web/Areas/Admin/Controllers/ProfilController.cs
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/ProfilController.cs
@@ -21,12 +21,27 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
         }
         public async Task< IActionResult> Index()
         {
-          var appuser= await  _userManager.FindByNameAsync(User.Identity.Name);
+            TempData["Active"] = "profil";
+            var appuser= await  _userManager.FindByNameAsync(User.Identity.Name);
             AppUserListViewModel model = new AppUserListViewModel();
             model.Id = appuser.Id;
             model.Name = appuser.Name;
             model.SurName = appuser.Surname;
             model.Email = appuser.Email;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserListViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var guncelenecekKullanici = _userManager.Users.FirstOrDefault(x => x.Id == model.Id);
+                guncelenecekKullanici.Name = model.Name;
+                guncelenecekKullanici.Surname = model.SurName;
+                guncelenecekKullanici.Email = model.Email;
+                await _userManager.UpdateAsync(guncelenecekKullanici);
+                return RedirectToAction("Index");
+            }
             return View(model);
         }
     }
