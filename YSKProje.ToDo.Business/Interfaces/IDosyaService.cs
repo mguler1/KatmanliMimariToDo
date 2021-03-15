aade3f1d9e7d5cd9420f8daebe3c68b2df7cf754
= <=>
2021-03-15 18:24:24 +0300

    Excel ve Pdf aktarma işlemleri için Business katmanına interface ve servis kodlamaları yapıldı

---
+++ YSKProje.ToDo.Business/Interfaces/IDosyaService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSKProje.ToDo.Business.Interfaces
+{
+    public interface IDosyaService
+    {
+        string AktarPdf<T>(List<T> list) where T:class,new();//dosya yolunu döndürecek
+        byte[] AktarExcel<T>(List<T> list) where T : class, new();//excel verisini byte şeklinde döndürecek
+
+    }
+}
