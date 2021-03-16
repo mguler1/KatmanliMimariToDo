86a03b0ab4f44393e10625cfc0718c6aff213e72
= <=>
2021-03-16 11:39:47 +0300

    Member Layout düzenlendi

---
+++ YSKProje.ToDo.Web/ViewComponents/Wrapper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YSKProje.ToDo.Entities.Concrete;
+using YSKProje.ToDo.Web.Areas.Admin.Models;
+
+namespace YSKProje.ToDo.Web.ViewComponents
+{
+    public class Wrapper : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public Wrapper(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IViewComponentResult Invoke()
+        {
+
+            var user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            AppUserListViewModel model = new AppUserListViewModel();
+            model.Id = user.Id;
+            model.Name = user.Name;
+            model.SurName = user.Surname;
+            model.Email = user.Email;
+            var roles = _userManager.GetRolesAsync(user).Result;
+            if (roles.Contains("Admin"))
+            {
+                return View(model);
+            }
+            return View("Member", model);
+        }
+    }
+}
