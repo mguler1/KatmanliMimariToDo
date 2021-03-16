1512cbe7d4d2ff9949f1290d34730e7b16e3d3b5
= <=>
2021-03-16 13:48:52 +0300

    Member profil ekranı tasarımı yapıldı

---
+++ YSKProje.ToDo.Web/Areas/Member/Controllers/ProfilController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YSKProje.ToDo.Entities.Concrete;
+using YSKProje.ToDo.Web.Areas.Admin.Models;
+
+namespace YSKProje.ToDo.Web.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [Authorize(Roles = "Member")]
+    public class ProfilController : Controller
+    {
+      private readonly  UserManager<AppUser> _userManager;
+        public ProfilController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task< IActionResult> Index()
+        {
+            TempData["Active"] = "profil";
+            var appuser= await  _userManager.FindByNameAsync(User.Identity.Name);
+            AppUserListViewModel model = new AppUserListViewModel();
+            model.Id = appuser.Id;
+            model.Name = appuser.Name;
+            model.SurName = appuser.Surname;
+            model.Email = appuser.Email;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserListViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var guncelenecekKullanici = _userManager.Users.FirstOrDefault(x => x.Id == model.Id);
+                guncelenecekKullanici.Name = model.Name;
+                guncelenecekKullanici.Surname = model.SurName;
+                guncelenecekKullanici.Email = model.Email;
+                await _userManager.UpdateAsync(guncelenecekKullanici);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
