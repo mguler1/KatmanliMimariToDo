b9af8696720c490309af803011e8f23d1b5f260e
= <=>
2021-03-19 12:47:41 +0300

    Member GorevController refactore edildi

--- YSKProje.ToDo.Web/Areas/Member/Controllers/GorevController.cs
+++ YSKProje.ToDo.Web/Areas/Member/Controllers/GorevController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YskProje.Todo.DTO.DTOs.GorevDto;
 using YSKProje.ToDo.Business.Interfaces;
 using YSKProje.ToDo.Entities.Concrete;
 using YSKProje.ToDo.Web.Areas.Admin.Models;
@@ -17,33 +19,24 @@ namespace YSKProje.ToDo.Web.Areas.Member.Controllers
     {
         private readonly IGorevService _gorevService;
         private readonly UserManager<AppUser> _userManager;
-        public GorevController(IGorevService gorevService, UserManager<AppUser> userManager)
+        private readonly IMapper _mapper;
+        public GorevController(IGorevService gorevService, UserManager<AppUser> userManager,IMapper mapper)
         {
             _gorevService = gorevService;
             _userManager = userManager;
+            _mapper = mapper;
         }
         public async Task< IActionResult> Index(int aktifSatfa=1)
         {
             TempData["Active"] = "gorev";
             var user=  await _userManager.FindByNameAsync(User.Identity.Name);
             int toplamsayfa;
-          var gorevler= _gorevService.GetirTumTablolarlaTamamlanmayan(out toplamsayfa,user.Id,aktifSatfa);
+         
+           
+           var gorevler= _mapper.Map<List<GorevAllListDto>>(_gorevService.GetirTumTablolarlaTamamlanmayan(out toplamsayfa, user.Id, aktifSatfa));
             ViewBag.ToplamSayfa = toplamsayfa;
             ViewBag.AktifSayfa = aktifSatfa;
-           List< GorevListAllViewModel> models = new List< GorevListAllViewModel>();
-            foreach (var gorev in gorevler)
-            {
-                GorevListAllViewModel model = new GorevListAllViewModel();
-                model.Id = gorev.Id;
-                model.Aciklama = gorev.Aciklama;
-                model.Aciliyet = gorev.Aciliyet;
-                model.Ad = gorev.Ad;
-                model.AppUser = gorev.AppUser;
-                model.OlusturulmaTarih = gorev.OlusturulmaTarih;
-                model.Raporlar = gorev.Raporlar;
-                models.Add(model);
-            }
-            return View(models);
+            return View(gorevler);
         }
     }
 }
